5ace2dde80a20c980978a56c81861eb0bc0db6f9
ekta <ektamo0123@gmail.com>
2022-08-12 14:28:52 +0530

    Added : Check Factors

---
+++ factors.java
@@ -0,0 +1,22 @@
+package CoreJava;
+import java.util.Scanner; 
+public class factors {
+
+	public static void main(String[] args) {
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		num = sc.nextInt();
+		
+		for(int i = 2; i< num; i++) {
+			while(num%i == 0) {
+				System.out.println("factors are: "+i+" ");
+				num = num/i;
+			}
+		}
+		if(num >2) {
+			System.out.println(num);
+		}
+	}
+
+}
